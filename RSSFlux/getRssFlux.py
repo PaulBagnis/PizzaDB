f37eee993ae2b25c039db3f9f1dbbaa0d7bb0025
Paul <paul.bagnis@gmail.com>
2022-01-20 15:22:34 +0100

    [add] error handling on certificates

--- RSSFlux/getRssFlux.py
+++ RSSFlux/getRssFlux.py
@@ -1,8 +1,11 @@
 import feedparser
 from pymongo import MongoClient
 import time
+import ssl
+import urllib
 
 #  TO DO : Utiliser BeautifulSoup pour parse le HTML et récuperer que ce qui peut nous interesser
+#          Passer sur ElasticSearch
 
 client = MongoClient("localhost", 27017)
 db = client.RSSFlux
@@ -12,7 +15,6 @@ screenrant = db.screenrant
 
 urls = {
     "allocine": [
-        "http://rss.allocine.fr/ac/actualites/cine",
         "http://rss.allocine.fr/ac/cine/cettesemaine",
         "http://rss.allocine.fr/ac/cine/alaffiche",
     ],
@@ -32,6 +34,13 @@ def getArticlesFromRSS():
         nbOfNewArticles = 0
         for url in urls[source]:
             Feed = feedparser.parse(url)
+            # Gestion des problèmes de certificats lors de la requête HTTP
+            try :
+                if isinstance(Feed.bozo_exception, urllib.error.URLError):
+                    ssl._create_default_https_context = ssl._create_unverified_context
+                    Feed = feedparser.parse(url)
+            except :
+                continue
             articles = []
             for entry in Feed.entries:
                 if alreadyExists(source, entry["id"]):
