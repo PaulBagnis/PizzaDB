f0554b4f2e95cf90e080f70b6039fa946585133e
Frantkich <francoisguern78@gmail.com>
2022-01-24 23:26:40 +0100

    [edit] simplify code

--- app/api/app.py
+++ app/api/app.py
@@ -2,31 +2,30 @@ from flask import Flask
 import os
 import time
 
-app = Flask(__name__)
+
 MAX_RETRIES = 30
 
+app = Flask(__name__)
 
-@app.route("/createHDFSDir")
+
+@app.route('/createHDFSDir')
 def createHDFSDir():
-    nbOfTries = 0
-    while str(os.system("hdfs dfs -mkdir /FilmPosters")) != "256":
-        if nbOfTries == MAX_RETRIES:
-            return "0"
-        time.sleep(1)
-        nbOfTries += 1
-    return "256"
+    return execFunction('hdfs dfs -mkdir /FilmPosters')
 
 
-@app.route("/loadToHDFS")
+@app.route('/loadToHDFS')
 def loadToHDFS():
+    return execFunction('hdfs dfs -copyFromLocal /home/*.jpg /FilmPosters')
+
+
+def execFunction(cmd):
     nbOfTries = 0
-    while str(os.system("hdfs dfs -copyFromLocal /home/*.jpg /FilmPosters")) != "256":
-        if nbOfTries == MAX_RETRIES:
-            return "0"
+    while str(os.system(cmd)) != 256:
         time.sleep(1)
         nbOfTries += 1
-    return "256"
+        if nbOfTries == MAX_RETRIES: return 0
+    return 256
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
