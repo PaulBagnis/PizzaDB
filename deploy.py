ba9f12e442423a9152347fac1f9baf8754469bd7
Paul <paul.bagnis@gmail.com>
2022-01-25 23:52:28 +0100

    [edit] Oublié qu'au final on a pas de Mongo, temps pour dormir ?

--- deploy.py
+++ deploy.py
@@ -34,25 +34,6 @@ def startES():
         print("\tFailed to connect to ElasticSearch Database, try again you sheep !\n")
 
 
-def startMongo():
-    """
-    DESC : Starts Mongo Database and tries request to be shure database is running
-    """
-    os.startfile(os.path.normpath(PATH_TO_MONGO_EXE))
-    count = 0
-    while count < MAX_RETRIES:
-        response = requests.get("http://localhost:27017/")
-        if response.status_code == 200:
-            print("\tMongoDB started !\n")
-            break
-        else:
-            print("Failed to connect to MongoDB, next attempt in 10 seconds...")
-            time.sleep(10)
-            count += 1
-    if count == MAX_RETRIES:
-        print("Failed to connect to MongoDB Database, try again you sheep !")
-
-
 os.system(CLEAR_SYNTAXE)
 
 # launch docker
@@ -69,11 +50,29 @@ print("\t######################\n\tDocker Started")
 # Start of ElasticSearch
 print("\tStarting ElasticSearch\n\t######################\n")
 startES()
-print("\t#####################\n\tElasticSearch Started")
-
-# Start of MongoDB
-print("\tStarting MongoDB\n\t#####################\n")
-startMongo()
-print("\t###############\n\tMongoDB Started\n\t###############\n")
+print("\t#####################\n\tElasticSearch Started#####################")
 
 input('\n\tDeployment done. Press a key and type "python app.py" to enjoy app')
+
+# def startMongo():
+#     """
+#     DESC : Starts Mongo Database and tries request to be shure database is running
+#     """
+#     os.startfile(os.path.normpath(PATH_TO_MONGO_EXE))
+#     count = 0
+#     while count < MAX_RETRIES:
+#         response = requests.get("http://localhost:27017/")
+#         if response.status_code == 200:
+#             print("\tMongoDB started !\n")
+#             break
+#         else:
+#             print("Failed to connect to MongoDB, next attempt in 10 seconds...")
+#             time.sleep(10)
+#             count += 1
+#     if count == MAX_RETRIES:
+#         print("Failed to connect to MongoDB Database, try again you sheep !")
+
+# # Start of MongoDB
+# print("\tStarting MongoDB\n\t#####################\n")
+# startMongo()
+# print("\t###############\n\tMongoDB Started\n\t###############\n")
